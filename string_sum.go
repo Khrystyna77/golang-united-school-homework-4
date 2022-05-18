90796bbdbe457c16c1034b59f8bcb78eb1359f23
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-18 19:40:21 +0300

    test3

--- string_sum.go
+++ string_sum.go
@@ -31,30 +31,43 @@ func StringSum(input string) (output string, err error) {
 		//fmt.Println(input, err.Error())
 		return "", err
 	}
-	if len(input) < 2 {
-		err := fmt.Errorf("should be at least 2 signs: %w", errorEmptyInput)
-		//fmt.Println(input, err.Error())
-		return "", err
+	//if len(input) <= 2 {
+	//	err := fmt.Errorf("should be at least 2 signs: %w", errorEmptyInput)
+	//fmt.Println(input, err.Error())
+	//	return "", err
+	//}
+	if len(input) == 0 || input == "" || input == " " {
+		err := fmt.Errorf("input is empty: %w", errorEmptyInput)
+		fmt.Println(input, err.Error())
+		//return "", err
 	}
+	if len(input) < 2 {
+		err := fmt.Errorf("should be at least 2 signs: %w", errorNotTwoOperands)
+		fmt.Println(input, err.Error())
+	}
+
 	if len(input) >= 2 {
 		var sum int
-		for _, x := range input {
 
+		for _, x := range input {
 			xstr := string(x) //x is rune converted to string
 			xint, _ := strconv.Atoi(xstr)
 			sum += xint
 
 		}
+
 		output := strconv.Itoa(sum)
-		fmt.Printf("%s", output)
-		err := fmt.Errorf("sum is not success: %w", errorEmptyInput)
-		//fmt.Println(input, err.Error())
-		return "", err
+		//fmt.Printf("%s", output)
+		err := fmt.Errorf("sum is not correct: %w", errorNotTwoOperands)
+		if err == nil {
+
+			fmt.Println(err.Error())
+		}
+
+		fmt.Println(output, nil)
+
 	}
-
-	//don't touch	if len(input) < 2 {
-
-	//return "", err
-
 	return "", nil
+	//end
+	//	return "", nil
 }
