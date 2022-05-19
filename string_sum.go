8f6e1cc2d0843d71fec82f2dbbc27b52577d0ee3
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-19 22:46:05 +0300

    test6

--- string_sum.go
+++ string_sum.go
@@ -40,34 +40,118 @@ func StringSum(input string) (output string, err error) {
 	//}
 
 	//sum
-	if len(input) == 1 {
+	if len(input) <= 1 {
 		err := fmt.Errorf("should be at least 2 signs: %w", errorNotTwoOperands)
 		fmt.Println(nil, err.Error())
 	}
 
 	//signPlus := strings.Contains(input, sign)
-	Res1count := strings.Count(input, "+")
+	signplus := "+"
+	signPlus1 := strings.Contains(input, signplus)
+	signminus := "-"
+	Signmm := strings.Contains(input, signminus)
 
-	if Res1count >= 2 {
-		//fmt.Println(input)
-		err := fmt.Errorf("more than 2 argument:: %w", errorEmptyInput)
-		fmt.Println(err.Error())
+	//19.05
+	Res1count := strings.Count(input, "+")
+	Res2count := strings.Count(input, "-")
+
+	//c, f
+	charec1 := strings.Contains(input, "c")
+	charec2 := strings.Contains(input, "f")
+	if charec1 == true || charec2 == true {
+		err := fmt.Errorf("contain letter: %w", errorNotTwoOperands)
+		fmt.Println(nil, err.Error())
 
 	}
 
-	//sum
-	//var sum int
-	if Res1count < 2 {
+	//42
+	if signPlus1 == false && Res1count == 0 {
+		err := fmt.Errorf("only 1 argument: %w", errorNotTwoOperands)
+		fmt.Println(nil, err.Error())
+
+	}
+
+	//--
+	if Signmm == true && Res2count >= 3 {
+		err := fmt.Errorf("more than 2 argument: %w", errorNotTwoOperands)
+		fmt.Println(nil, err.Error())
+	}
+
+	if Signmm == true && Res2count == 1 {
+		min := strings.Split(input, "-")
+		min1 := min[0]
+		min2 := min[1]
+		min1m, _ := strconv.Atoi(min1)
+		min2m, _ := strconv.Atoi(min2)
+		if min2m > min1m {
+			sumintm := min2m - min1m
+
+			Result3 := strconv.Itoa(sumintm)
+			fmt.Println("-" + Result3)
+
+		}
+		if min2m < min1m {
+			sumintm := min1m + min2m
+
+			Result3 := strconv.Itoa(sumintm)
+			fmt.Println("-" + Result3)
+
+		}
+
+		err := fmt.Errorf("sum minus is not correct: %w", errorNotTwoOperands)
+		if err == nil {
+
+			fmt.Println(err.Error())
+
+			//fmt.Println(Result3)
+		}
+
+		//fmt.Println("-"+Result3, nil)
+		//retirn "", err
+
+	}
+
+	//two minus
+	if Signmm == true && Res2count < 3 && Res2count > 1 {
+		partsm := strings.Split(input, "-")
+		part1m := partsm[1]
+		part2m := partsm[2]
+
+		sint1m, _ := strconv.Atoi(part1m)
+		sint2m, _ := strconv.Atoi(part2m)
+		sumintm := sint1m + sint2m
+
+		outputm := strconv.Itoa(sumintm)
+		//fmt.Printf("%s","-" + outputm)
+		err := fmt.Errorf("sum is not correct: %w", errorNotTwoOperands)
+		if err == nil {
+
+			fmt.Println(err.Error())
+		}
+
+		fmt.Println("-"+outputm, nil)
+		//retirn "", err
+
+	}
+
+	//+
+	if signPlus1 == true && Res1count >= 2 {
+		err := fmt.Errorf("more than 2 argument: %w", errorNotTwoOperands)
+		fmt.Println(nil, err.Error())
+	}
+
+	if signPlus1 == true && Res1count < 2 {
+
 		parts := strings.Split(input, "+")
 		part1 := parts[0]
 		part2 := parts[1]
+
 		sint1, _ := strconv.Atoi(part1)
 		sint2, _ := strconv.Atoi(part2)
-		//sum
 		sumint := sint1 + sint2
 		//fmt.Println(sumint)
 
-		result := strconv.Itoa(sumint)
+		output := strconv.Itoa(sumint)
 		//fmt.Printf("%s", output)
 		err := fmt.Errorf("sum is not correct: %w", errorNotTwoOperands)
 		if err == nil {
@@ -75,8 +159,8 @@ func StringSum(input string) (output string, err error) {
 			fmt.Println(err.Error())
 		}
 
-		fmt.Println(result, nil)
-
+		fmt.Println(output, nil)
+		//retirn "", err
 	}
 	//end
 	return "", nil
